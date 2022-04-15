7a90588300be2c9626b45f7a265499de9c27c2f0
MazharSolkar <mazharsolkar65@gmail.com>
2022-04-15 13:57:23 +0530

    mvc project practical 01

---
+++ practical-1/MyMVC/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using myMvc.Models;
+
+namespace MyMVC.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        var model = new StockQuote{Symbol="HLLO",Price=3200,By="Mazhar Solkar"};
+        return View(model);
+    }
+
+}
---
+++ practical-1/MyMVC/Models/StockQuote.cs
@@ -0,0 +1,8 @@
+namespace myMvc.Models;
+    public class StockQuote
+    {
+        public string? Symbol {get;set;}
+        public int Price {get;set;}
+        public string? By {get;set;}
+    }
+
